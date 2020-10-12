79f08da72e1781910658ffbeb0fc2d81eafdfa69
ethan <emlyons@protonmail.com>
2020-10-12 16:10:28 +1030

    implemented FatEntry

--- lib/fat32/src/vfat/fat.rs
+++ lib/fat32/src/vfat/fat.rs
@@ -25,7 +25,34 @@ pub struct FatEntry(pub u32);
 impl FatEntry {
     /// Returns the `Status` of the FAT entry `self`.
     pub fn status(&self) -> Status {
-        unimplemented!("FatEntry::status()")
+	// 28-bits of FAT entry are used
+	let status = self.0 & 0xFFFFFFF;
+
+	if status == 0x00 {
+	    return Status::Free;
+	}
+
+	if status == 0x01 {
+	    return Status::Reserved;
+	}
+
+	if 0x02 <= status && status <= 0xFFFFFEF {
+	    return Data(Cluster::from(self.0));
+	}
+
+	if 0xFFFFFF0 <= status && status <= 0xFFFFFF6 {
+	    return Reserved;
+	}
+
+	if status == 0xFFFFFF7 {
+	    return Bad;
+	}
+
+	if 0xFFFFFF8 <= status && status <= 0xFFFFFFF {
+	    return Eoc(self.0);
+	}
+
+	unreachable!()
     }
 }
 
